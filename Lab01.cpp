a5b87adaf34ab849b1a333ed304cefc247cc7604
Krishna Sai C <kchemudu@uwyo.edu>
2017-09-16 22:50:51 -0600

    Lab01.cpp
    For some reason i couldn't find the Lab01 folder inside Lab01 parent folder which contains the .cpp source code file. I'm uploading it now.

---
+++ Lab01.cpp
@@ -0,0 +1,77 @@
+//Krishna Sai Chemudupati
+//COSO 2030
+//9/11/2017
+// Lab 01
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string filename;
+	string numbers;
+	int counter =0;
+	int finalCount;
+	string n1, n2, n3, n4;
+	cout << "Enter a file name: ";
+	cin >> filename;
+	ifstream myfile(filename);
+	if (myfile.is_open())
+	{
+
+		while (!myfile.eof())
+		{
+			if(myfile.fail())
+			{
+				myfile.ignore();
+				myfile.clear();
+			}
+			else
+			{
+				counter++;
+				cin >> numbers;
+				if (counter == 1)
+				{
+					n1 = numbers;
+				}
+				else if (counter == 2)
+				{
+					n2 = numbers;
+				}
+			}
+		}
+		finalCount = counter;
+		counter = 0;
+		while (!myfile.eof())
+		{
+			if (myfile.fail())
+			{
+				myfile.ignore();
+				myfile.clear();
+			}
+			else
+			{
+				counter++;
+				cin >> numbers;
+				if (counter == (finalCount-1))
+				{
+					n3 = numbers;
+				}
+				else if (counter == finalCount)
+				{
+					n4 = numbers;
+				}
+			}
+		}
+	}
+	else
+	{
+		cout << "Error! Cannot open the file. Please check the file name." << endl;
+	}
+	cout << "The total number of numbers in the file are: " << finalCount << endl;
+	cout << "The first two numbers are: " << n1 << ", " << n2 << endl;
+	cout << "The last two numbers are: " << n3 << ", " << n4 << endl;
+	return 0;
+}
